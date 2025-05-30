0794c8e09dd782f233e52094cb67f22095eda83c
Pedro Jaber <pedrojaberc@gmail.com>
2025-05-30 18:55:09 -0300

    feat: implent seller finfById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,16 @@
 package application;
 
-import java.util.Date;
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        System.out.println(department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, department);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
